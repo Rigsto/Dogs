f8f76a91fd34fb7858f346ddfd0ac83d1494fc6f
Rigsto <rigsto@users.noreply.github.com>
2020-01-06 00:01:05 +0700

    clean up for next project

--- app/src/main/java/com/masterkotlin/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/masterkotlin/dogs/view/DetailFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.masterkotlin.dogs.R
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
     private var dogUuid = 0
@@ -18,19 +16,5 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        arguments?.let {
-            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
-            textView2.text = dogUuid.toString()
-        }
-
-        buttonList.setOnClickListener {
-            val action = DetailFragmentDirections.actionListFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
     }
 }
