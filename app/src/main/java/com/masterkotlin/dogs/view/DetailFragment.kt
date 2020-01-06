decf60547459c4c89abd4c6fa0556de1c69c7ff3
Rigsto <rigsto@users.noreply.github.com>
2020-01-06 13:53:20 +0700

    mvvm and livedata

--- app/src/main/java/com/masterkotlin/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/masterkotlin/dogs/view/DetailFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.masterkotlin.dogs.R
+import com.masterkotlin.dogs.viewmodel.DetailViewModel
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
-    private var dogUuid = 0
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +20,25 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.dogLiveData.observe(this, Observer { dog ->
+            dog?.let {
+                dogName.text = dog.dogBreed
+                dogPurpose.text = dog.bredFor
+                dogTemperament.text = dog.temperament
+                dogLifespan.text = dog.lifeSpan
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/masterkotlin/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,36 @@
+package com.masterkotlin.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.masterkotlin.dogs.R
+import com.masterkotlin.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+    fun updateDogList(newDogsList: List<DogBreed>){
+        dogsList.clear()
+        dogsList.addAll(newDogsList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.view.name.text = dogsList[position].dogBreed
+        holder.view.lifespan.text = dogsList[position].lifeSpan
+        holder.view.setOnClickListener {
+            Navigation.findNavController(it).navigate(ListFragmentDirections.actionDetailFragment())
+        }
+    }
+
+    override fun getItemCount(): Int = dogsList.size
+
+    inner class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+}
